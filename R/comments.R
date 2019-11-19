f44189ef909efb97272671629fecef60e4ad2f49
Swechhya <swechhyabista@gmail.com>
2019-11-19 14:44:50 +0545

    Add R function to get comments

--- R/comments.R
+++ R/comments.R
@@ -7,20 +7,46 @@
 #' @param comment the comment that is to be added to the cell
 #' @examples
 #' if(interactive()) {
-#' library(shiny)
-#' library(excelR)
-#' shinyApp(
-#'  ui = fluidPage(excelOutput("table", height = 175), actionButton('comment', 'Set Comments to cell A1')),
-#'  server = function(input, output, session) {
-#'      output$table <- renderExcel(excelTable(data = head(iris)))
-#'      observeEvent(input$comment,{
-#'          setComments("table", "A1", "This is a comment")
-#'      })
-#'    }
-#'  )
-#' }
+#'  library(shiny)
+#'  library(excelR)
+#'  shinyApp(
+#'   ui = fluidPage(excelOutput("table", height = 175), actionButton('comment', 'Set Comments to cell A1')),
+#'      server = function(input, output, session) {
+#'          output$table <- renderExcel(excelTable(data = head(iris)))
+#'          observeEvent(input$comment,{
+#'              setComments("table", "A1", "This is a comment")
+#'          })
+#'      }
+#'      )
+#'  }
 setComments <- function(tableId, cellId, comment) {
-
+  session <- shiny::getDefaultReactiveDomain()
   session$sendCustomMessage("excelR:setComments", message=list(tableId, cellId, comment))
+}
 
+#'
+#' This function is used to add comment to the specified cell
+#' @export
+#' @param tableId the id of the table from which the comment is to be fetched
+#' @param cellId the id of the cell from which the comment is to be fetched
+#' @examples
+#' if(interactive()) {
+#'  library(shiny)
+#'  library(excelR)
+#'  shinyApp(
+#'      ui = fluidPage(excelOutput("table", height = 175), actionButton('comment', 'Get Comments from cell A1')),
+#'      server = function(input, output, session) {
+#'          output$table <- renderExcel(excelTable(data = head(iris)))
+#'          observeEvent(input$comment, {
+#'              getComments("table", "A1")
+#'          })
+#'          observeEvent(input$table, {
+#'          print(input$table$comment)
+#'          })
+#'      }
+#'      )
+#'  }
+getComments <- function(tableId, cellId) {
+  session <- shiny::getDefaultReactiveDomain()
+  session$sendCustomMessage("excelR:getComments", message=list(tableId, cellId))
 }

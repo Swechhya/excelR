b69354b483bd7277c3486b92fc9a1f64b0eaab92
Swechhya <swechhyabista@gmail.com>
2019-11-19 14:45:57 +0545

    Add get comment folder in js side

--- inst/htmlwidgets/jexcel.js
+++ inst/htmlwidgets/jexcel.js
@@ -160,4 +160,18 @@
         el.excel.setComments(message[1], message[2]);
       }
     });
+
+    // This function is used to get comments  from table
+    Shiny.addCustomMessageHandler("excelR:getComments", function(message) {
+
+      var el = document.getElementById(message[0]);
+      if (el) {
+        var comments = message[1] ? el.excel.getComments(message[1]): el.excel.getComments(null);
+
+        Shiny.setInputValue(message[0], 
+          {
+           comments
+          });
+      }
+    });
   }

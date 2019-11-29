4798f56f935725b5f3c03d560b228dc8b0abf803
Swechhya <swechhyabista@gmail.com>
2019-11-29 14:16:37 +0545

    Fix test

--- tests/testthat/test_selected_data.R
+++ tests/testthat/test_selected_data.R
@@ -9,5 +9,5 @@ test_that("'get_selected_data' return null when excelObj is null ", {
 test_that ("'get_selected_data' return a data frame when excelObj is not null", {
   data <- matrix(1:50, ncol=5)
   excelObj <- list(data=c(unname(as.data.frame(data))), colHeaders=as.list(c(rep("",5))))
-  testthat::expect_s3_class(excel_to_R(excelObj), "data.frame")
+  testthat::expect_s3_class(get_selected_data(excelObj), "data.frame")
 })

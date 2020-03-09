e78b12afd36d09a983cf36da31975678a06e022e
Swechhya <swechhyabista@gmail.com>
2020-03-09 16:17:05 +0545

    Add function to get selected cell boundaty values

--- R/get_selected_data.R
+++ R/get_selected_data.R
@@ -1,6 +1,6 @@
 #' Get selected cells from excel table
 #'
-#' This function is used to the data selected in excel table
+#' This function is used to get the data selected in excel table
 #' @export
 #' @param excelObj the json data retuned from excel table
 #' @examples
@@ -29,4 +29,32 @@ get_selected_data<- function(excelObj) {
       return(dataOutput)
    }
 
+}
+
+#' Get selected cells boundary from excel table
+#'
+#' This function is used to the boundary points of data selected in excel table
+#' @export
+#' @param excelObj the json data retuned from excel table
+#' @examples
+#'if(interactive()){
+#'  library(shiny)
+#'  library(excelR)
+#'  shinyApp(
+#'    ui = fluidPage(excelOutput("table")),
+#'    server = function(input, output, session) {
+#'      output$table <-
+#'        renderExcel(excelTable(data = head(iris), getSelectedData = TRUE))
+#'      observeEvent(input$table,{
+#'        print(get_selected_data_boundary(input$table))
+#'      })
+#'    }
+#'  )
+#'}
+get_selected_data_boundary<- function(excelObj) {
+   if (!is.null(excelObj) && excelObj$forSelectedVals) {
+      boundaryIndex <- excelObj$selectedDataBoundary
+      boundaryCell = lapply(boundaryIndex, function(x){x+1})
+      return(boundaryCell)
+   }
 }

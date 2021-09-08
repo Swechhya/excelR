b700ac8c852823b56af3c381515e210eb19c2a45
Swechhya <swechhyabista@gmail.com>
2021-09-08 17:56:15 +0545

    Fix test

--- tests/testthat/test_getTableData.R
+++ tests/testthat/test_getTableData.R
@@ -1,11 +1,5 @@
 context("'getTableData' function")
 
-test_that("'getTableData' return null when excelObj is null ", {
-
-  testthat::expect_null(excel_to_R(getTableData(NULL)))
-})
-
-
 test_that("'getTableData' does not work outside shiny", {
   testthat::expect_error(getTableData("table"))
 })
